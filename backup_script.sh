ee1b91e08dcf633edc40579fafcf4adba6e7bb7c
MOHAMEDMAHFOUDH <42978596+MOHAMEDMAHFOUDH@users.noreply.github.com>
2018-09-04 22:25:53 +0200

    Update backup_script.sh

--- backup_script.sh
+++ backup_script.sh
@@ -1,1 +1,17 @@
 
+mkdir /home/user/save_BD
+vim backup_script.sh
+
+#!/bin/bash
+# On liste nos bases de données
+LISTEBDD=$( echo 'show databases' | mysql -u backup --password=< mot de passe >)
+for BDD in $LISTEBDD do
+# Exclusion des BDD information_schema , mysql et Database
+if [[ $BDD != "information_schema" ]] && [[ $BDD != "mysql" ]] && [[ $BDD != "Database" ]]; then
+# Emplacement du dossier ou nous allons stocker les bases de données, un dossier par base de données
+  CHEMIN=/home/user/save_BD/$BDD
+# On backup notre base de donnees
+  mysqldump -u backup --single-transaction --password= $BDD > "$CHEMIN/$BDD.sql"
+  echo "|Sauvegarde de la base de donnees $BDD.sql ";
+fi
+done
